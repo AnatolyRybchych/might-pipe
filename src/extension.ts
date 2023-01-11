9eb0b4e4c02330b72e3a1faff03483a86c7f3279
Anatoly Rybchych <tol.ryb@gmail.com>
2023-01-11 06:01:51 +0200

    remove waisting promise and displaying message about start of command execution

--- src/extension.ts
+++ src/extension.ts
@@ -3,48 +3,59 @@ import * as vscode from 'vscode';
 import childProcess = require('child_process');
 import { TextDecoder, TextEncoder } from 'util';
 
-function executeCommmand():Promise<void>{
-	return new Promise(()=>{
-		const textEditor = vscode.window.activeTextEditor;
-		const decoderUtf8 = new TextDecoder(); 
-		const showErrorMessage = vscode.window.showErrorMessage;
-		const outputChannel = vscode.window.createOutputChannel("might-pipe");
+function nCommands(n:number):string{
+	return `${n} command${n === 1? "" : "s"}`;
+}
 
-		if(!textEditor){
-			return;
-		}
+function executeCommmand():void{
+	const textEditor = vscode.window.activeTextEditor;
+	const decoderUtf8 = new TextDecoder(); 
+	const showErrorMessage = vscode.window.showErrorMessage;
+	const showInformationMessage = vscode.window.showInformationMessage;
+	const outputChannel = vscode.window.createOutputChannel("might-pipe");
 
-		textEditor.edit((editBuffer)=>{
-			textEditor.selections.forEach((selection)=>{
-				const text = textEditor.document.getText(selection).trim();
+	if(!textEditor){
+		return;
+	}
 
-				if(text === ""){
-					return;
-				}
-				outputChannel.show(true);
-				outputChannel.append(`$${text}\n\n`);
-				try {
-					const proc = childProcess.execSync(text);
-					const procOutput = decoderUtf8.decode(proc.buffer).trim();
+	showInformationMessage(`Executing ${nCommands(textEditor.selections.length)}`);
 
-					editBuffer.replace(selection, procOutput);
+	textEditor.edit((editBuffer)=>{
+		textEditor.selections.forEach((selection)=>{
+			const text = textEditor.document.getText(selection).trim();
 
-					outputChannel.append(`${procOutput}\n\n`);
+			if(text === ""){
+				return;
+			}
+			outputChannel.show(true);
+			outputChannel.append(`$${text}\n\n`);
+			try {
+				const proc = childProcess.execSync(text);
+				const procOutput = decoderUtf8.decode(proc.buffer).trim();
 
-				} catch (error) {
-					showErrorMessage(`Feiled execution "${text}"`);
-					outputChannel.append(`${error}\n\n`);
-				}
-			});
+				editBuffer.replace(selection, procOutput);
+
+				outputChannel.append(`${procOutput}\n\n`);
+
+			} catch (error) {
+				showErrorMessage(`Feiled execution "${text}"`);
+				outputChannel.append(`${error}\n\n`);
+			}
 		});
 	});
 }
 
+function runCommand():void{
+	
+}
+
 export function activate(context: vscode.ExtensionContext) {
-
-	let disposable = vscode.commands.registerCommand('might-pipe.execute_commmand', executeCommmand);
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('might-pipe.execute_commmand', executeCommmand)
+	);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('might-pipe.run_command', executeCommmand)
+	);
 }
 
 export function deactivate() {}
